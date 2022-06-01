70890bdfeb814ebd0e400b54715b42f88dd0ca6d
Adrian <105984281+adeulia@users.noreply.github.com>
2022-05-31 19:30:05 -0500

    Adding interface and tokens
    Interface lets me break up services by following S in SOLID

---
+++ API/Interfaces/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using API.Entities;
+
+namespace API.Interfaces
+{
+    // interface is kinda like a contract between itself and any class that implements it
+    public interface ITokenService
+    {
+        string CreateToken(AppUser user);
+    }
+}
